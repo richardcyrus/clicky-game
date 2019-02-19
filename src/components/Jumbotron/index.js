a7bbfaf324241d76e10b909563642d5309365963
Richard Cyrus <richard.cyrus@rcyrus.com>
2019-02-19 17:01:20 -0500

    Create the Jumbotron component

---
+++ src/components/Jumbotron/index.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import './Jumbotron.css';
+
+class Jumbotron extends Component {
+  render() {
+    return (
+      <div className="jumbotron text-center rounded-0">
+        <div className="caption">
+          <h1 className="display-2">
+            Looney Tunes
+            <small>
+              <sup>&reg;</sup>
+            </small>{' '}
+            Clicky Game!
+          </h1>
+          <p className="instructions">
+            Click on an image to earn points. Do not click on any single image
+            more than once!
+          </p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Jumbotron;
