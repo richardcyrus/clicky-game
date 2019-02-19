ddaf232e8de36a3b439fa374b4fabd546f113101
Richard Cyrus <richard.cyrus@rcyrus.com>
2019-02-19 17:06:51 -0500

    Define the base state properties

--- src/App.js
+++ src/App.js
@@ -7,6 +7,41 @@ import Footer from './components/Footer';
 import characters from './characters';
 
 class App extends Component {
+  state = {
+    score: 0,
+    topScore: 0,
+    shake: false,
+    userMessage: {
+      message: 'Click an image to begin play!',
+      correct: false,
+      incorrect: false
+    },
+    characters: this.shuffle(characters)
+  };
+  /**
+   * A Fisher-Yates shuffle to ensure that grid is unique on each shuffle.
+   *
+   * @see https://bost.ocks.org/mike/shuffle/
+   * @see https://www.frankmitchell.org/2015/01/fisher-yates/
+   *
+   * @param list
+   * @returns {*}
+   */
+  shuffle(list) {
+    let pick;
+    let current;
+    let remaining = list.length;
+
+    while (remaining !== 0) {
+      pick = Math.floor(Math.random() * remaining--);
+
+      current = list[remaining];
+      list[remaining] = list[pick];
+      list[pick] = current;
+    }
+
+    return list;
+  }
   render() {
     return <div className="App" />;
   }
