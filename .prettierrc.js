0402fb62d11f0fce14834c430dd3073f5149ddd2
Richard Cyrus <richard.cyrus@rcyrus.com>
2019-02-18 20:33:54 -0500

    Update editor and linting configurations

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+/**
+ * clicky-game
+ *
+ * The Coding Boot Camp at UNC Charlotte.
+ * (c) 2019 Richard Cyrus <hello@rcyrus.com>
+ */
+
+module.exports = {
+  arrowParens: 'always',
+  singleQuote: true
+};
