8feb2869248bdb6a194f4449bddeb5608d3baca2
Richard Cyrus <richard.cyrus@rcyrus.com>
2019-02-20 22:27:14 -0500

    Updates

---
+++ precache-manifest.131b14d8e24705c0ef1cb8e9bb767af8.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "725a0fde28e712f69723",
+    "url": "/clicky-game/static/css/main.54e98ef4.chunk.css"
+  },
+  {
+    "revision": "725a0fde28e712f69723",
+    "url": "/clicky-game/static/js/main.725a0fde.chunk.js"
+  },
+  {
+    "revision": "7985e359b3121cf81761",
+    "url": "/clicky-game/static/js/runtime~main.7985e359.js"
+  },
+  {
+    "revision": "956ea3cdbbdeccacee77",
+    "url": "/clicky-game/static/css/2.14a13f28.chunk.css"
+  },
+  {
+    "revision": "956ea3cdbbdeccacee77",
+    "url": "/clicky-game/static/js/2.956ea3cd.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dcfe03190226fc3e1265c28761bbf4b5",
+    "url": "/clicky-game/static/media/05-01.dcfe0319.jpg"
+  },
+  {
+    "revision": "169d1eb3b55ff2b5f1cf0dcb6e2e44bf",
+    "url": "/clicky-game/index.html"
+  }
+];
