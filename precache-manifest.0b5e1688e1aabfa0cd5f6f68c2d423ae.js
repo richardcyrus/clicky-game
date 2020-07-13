8620ac1ccfb5f710bd5f8a96005fb53a37100d00
Richard Cyrus <richard.cyrus@rcyrus.com>
2020-07-13 13:50:39 -0400

    Updates

---
+++ precache-manifest.0b5e1688e1aabfa0cd5f6f68c2d423ae.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aad4f123aa0373bc10de72134b728a75",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "7d5bfd844f8355781b95",
+    "url": "/clicky-game/static/css/2.2c7d9c01.chunk.css"
+  },
+  {
+    "revision": "c4ba119310a19ca45b24",
+    "url": "/clicky-game/static/css/main.25faf5c5.chunk.css"
+  },
+  {
+    "revision": "7d5bfd844f8355781b95",
+    "url": "/clicky-game/static/js/2.d967fefe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.d967fefe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4ba119310a19ca45b24",
+    "url": "/clicky-game/static/js/main.fc8d285e.chunk.js"
+  },
+  {
+    "revision": "a2dd66fdceb11b57b463",
+    "url": "/clicky-game/static/js/runtime-main.3c5f3143.js"
+  },
+  {
+    "revision": "dcfe03190226fc3e1265c28761bbf4b5",
+    "url": "/clicky-game/static/media/05-01.dcfe0319.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/clicky-game/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/clicky-game/precache-manifest.12f82fc7412b44ea9795e526041f41d2.js"
+  "/clicky-game/precache-manifest.0b5e1688e1aabfa0cd5f6f68c2d423ae.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/clicky-game/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/clicky-game/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
